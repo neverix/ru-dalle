b6a4a0b8d707794ee8677030ecd87a51cb43fc01
neverix <stomperhomp@gmail.com>
2021-12-08 13:48:01 +0300

    Remove debug variable `img`
    Makes the model fail on non-square images.

--- rudalle/dalle/image_attention.py
+++ rudalle/dalle/image_attention.py
@@ -31,7 +31,6 @@ def get_conv_mask(text_tokens=256, image_tokens_per_dim=32, kernel=11, is_bool_m
     shift = kernel // 2
     for pos in range(text_tokens, mask.size(1)):
         mask[pos+1:, pos] = False if is_bool_mask else 0.0
-        img = torch.zeros(image_tokens_per_dim, image_tokens_per_dim)
         pixel_id = pos - text_tokens
         row = pixel_id // image_tokens_per_dim
         col = pixel_id % image_tokens_per_dim
@@ -39,10 +38,8 @@ def get_conv_mask(text_tokens=256, image_tokens_per_dim=32, kernel=11, is_bool_m
             for c in range(-shift, shift+1):
                 c_abs = (c + col) % image_tokens_per_dim
                 r_abs = (r + row) % image_tokens_per_dim
-                img[r_abs, c_abs] = 0.2
                 cell_id = r_abs * image_tokens_per_dim + c_abs
                 if text_tokens + cell_id > pos:
                     mask[text_tokens + cell_id, pos] = True if is_bool_mask else 1.0
 
-        img[row, col] = 1.0
     return mask
